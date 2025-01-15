66646e783b1d4e86539f27bd7d1d20bbb1582a14
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2025-01-15 23:47:58 +0700

    first commit

---
+++ src/main/java/com/hvv/agriservice/AgriServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hvv.agriservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hvv/agriservice/AgriServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvv.agriservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
